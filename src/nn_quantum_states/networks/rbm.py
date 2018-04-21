e593c98adb4c8bfb1ad11d971fd6a49aeb6b46f0
Andrew Gleeson <katanka.gleeson@gmail.com>
2018-04-21 10:53:20 -0700

    added sampling

--- src/nn_quantum_states/networks/rbm.py
+++ src/nn_quantum_states/networks/rbm.py
@@ -2,8 +2,10 @@ import numpy as np
 
 from nn_quantum_states.networks.neuralnetwork import NeuralNetwork
 
+
 def sigmoid(x):
     return np.pow((1 + np.exp(-x)), -1)
+
 
 class RBM(NeuralNetwork):
     def __init__(self, num_vis, num_hid):
@@ -17,8 +19,7 @@ class RBM(NeuralNetwork):
 
         self.eff_angles = np.zeros((num_hid, 1))
 
-        self.weight_matrix = np.zeros((num_vis, num_hid))
-
+        self.weights = np.zeros((num_vis, num_hid))
 
     # Computes the wave function amplitude corresponding to a
     # given n-dimensional state vector
@@ -28,25 +29,39 @@ class RBM(NeuralNetwork):
         b = (np.random.random(size) - .5) * 10e-2
         return a + b * 1j
 
-
     def effective_angles(self, state):
         return self.vis_bias + self.weight_matrix.T @ state
 
     def Psi_M(self, state):
-        return np.exp(self.vis_bias @ state) * np.prod(2*np.cosh(self.effective_angles(state)))
+        return np.exp(self.vis_bias @ state) * np.prod(2 * np.cosh(self.effective_angles(state)))
 
     def initialize_weights(self):
         self.vis_bias = self.random_complex((self.num_vis, 1))
         self.hid_bias = self.random_complex((self.num_hid, 1))
-        self.weight_matrix = self.random_complex((self.num_vis, self.num_hid))
-
+        self.weights = self.random_complex((self.num_vis, self.num_hid))
 
     def E_Local(self, state):
         E_loc = 0
         return E_loc
 
-    def Gibbs_step(self):
-        #flip random site
-        site = np.random.choice(range(self.num_vis), 1)
+    def sample(self):
+        spins = np.zeros((self.num_vis, 1))
+        hidden = np.zeros((self.num_hid, 1))
+        for i in range(self.num_vis):
+            gamma = sum([self.weights[i, j] * hidden[j] for j in range(self.num_hid)]) + self.vis_bias[i]
+            if sigmoid(2 * gamma) > np.random.normal():
+                spins[i] = 1
+            else:
+                spins[i] = -1
 
+        for j in range(self.num_hid):
+            theta = sum([self.weights[i, j] * spins[j] for j in range(self.num_vis)]) + self.hid_bias[j]
+            if sigmoid(2 * theta) > np.random.normal():
+                hidden[i] = 1
+            else:
+                hidden[i] = -1
 
+        return spins, hidden
+
+    # flip random site
+    # site = np.random.choice(range(self.num_vis), 1)
