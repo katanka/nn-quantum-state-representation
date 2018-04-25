35da1c9ebdc13cc4e89d42c9adab6f41b1e8ad97
Andrew Gleeson <katanka.gleeson@gmail.com>
2018-04-25 14:18:58 -0700

    move code from __init__ to main

--- src/nn_quantum_states/main.py
+++ src/nn_quantum_states/main.py
@@ -1,23 +1,30 @@
 from nn_quantum_states.networks.rbm import RBM
 from nn_quantum_states.hamiltonians.ising1d import Ising1D
 import matplotlib.pyplot as plt
+import numpy as np
 
 import sys
 sys.path.insert(0, './')
 
-num_spins = 5
-alpha = 2
 
-num_SR_steps = 50
-hami = Ising1D('Ising Model', num_spins, h=2)
-rbm = RBM(num_spins, alpha * num_spins, hami)
-Energies = []
-for _ in range(50):
-    E_loc = rbm.SR_step(iterations=10000, therm_factor=.1)
-    Energies.append(E_loc)
+num_spins = 4
+alpha = 2
+iter = 20000
+
+hamiltonian = Ising1D("ising1D", num_spins, h=1)
+model = RBM(num_spins, alpha*num_spins, hamiltonian)
+
+spins = np.random.randint(2, size=model.num_vis)
+spins[spins == 0] = -1
+
+model.init_effective_angles(spins.reshape((num_spins, 1)))
+
+E = []
+for i in range(30):
+    E.append(model.SR_step(iter, therm_factor=.01, reg=100*.9**i))
 
 plt.figure()
-plt.plot(range(50), Energies)
+plt.plot(range(30), E)
 plt.xlabel('Iteration')
 plt.ylabel('$E_{loc}$')
 plt.title('Energy vs. Iteration')
